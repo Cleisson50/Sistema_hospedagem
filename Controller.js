021bf7b350e2ee4e25b9abd6b253e6167da1e9c2
Cleisson <62613949+Cleisson50@users.noreply.github.com>
2022-09-08 21:04:17 -0300

    first commit

---
+++ Controller.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const model = require('./models/user');
+
+let app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Routes
+app.post('/create', async (req, res) => {
+    let reqs = await model.User.create({
+        'name': req.body.nameUser,
+        'password': req.body.passwordUser,
+        'email': req.body.emailUser,
+        'balance': 1000,
+        'createdAt': new Date(),
+        'updatedAt': new Date()
+    });
+    if (reqs) {
+        res.send(JSON.stringify('O usuário foi cadastrado com sucesso!'));
+    }
+});
+
+//Start server
+let port = process.env.PORT || 3000;
+app.listen(port, (req, res) => {
+    console.log('Servidor Rodando');
+});
